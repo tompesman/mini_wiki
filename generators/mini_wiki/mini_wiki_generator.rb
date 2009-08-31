201e10534bb20eaa1a034eb2e80af49ddd4784bb
tompesman <tom@tnux.net>
2009-08-31 08:44:27 +0200

    Readme updated, Usage updated, generator installs now the required files, and moved the example files to the generator directory

--- generators/mini_wiki/mini_wiki_generator.rb
+++ generators/mini_wiki/mini_wiki_generator.rb
@@ -1,8 +1,17 @@
-class MiniWikiGenerator < Rails::Generator::NamedBase
+class MiniWikiGenerator < Rails::Generator::Base
   def manifest
     record do |m|
       # add the migration
-      m.migration_template 'add_mini_wiki.rb', "db/migrate", { :migration_file_name => "add_mini_wiki" }  
+      m.migration_template 'add_mini_wiki.rb', 'db/migrate', { :migration_file_name => 'add_mini_wiki' }  
+      
+      # layout
+      m.file 'layouts/mini_wiki.html.erb', 'app/views/mini_wiki.css'
+
+      # css
+      m.file 'css/mini_wiki.css', 'public/stylesheets/mini_wiki.css'
+
+      # config
+      m.file 'config/mini_wiki.yml', 'config/mini_wiki.yml'
     end
   end
 end
